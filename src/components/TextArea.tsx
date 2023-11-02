2d12833e8e1ae0ea7a5c682fc191b423fc07c9f0
Ariel Salcito <ariel_salcito@live.com>
2023-11-01 22:44:29 -0300

    Working

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,6 +1,11 @@
 import { Form } from 'react-bootstrap'
+import { SectionType } from '../types'
 
-export const TextArea = () => {
+type Props =
+  | {type: SectionType.From, loading?: undefined, onChange: (value: string) => void, value: string}
+  | {type: SectionType.To, loading?: boolean, onChange: (value: string) => void, value: string}
+
+export const TextArea = ({loading, type, value, onChange}) => {
     return (
         <Form.Control 
         as='textarea'
